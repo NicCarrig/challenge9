78029810ca1f8da69f0ae7a3fe96a724fb9a7c83
unknown <clemcarrig90@hotmail.com>
2022-05-27 21:30:22 -0700

    updated file setup

---
+++ index.js
@@ -0,0 +1,46 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown')
+
+// TODO: Create an array of questions for user input
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the name of your project?'
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Please enter a description of your problem'
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'What packages/dependencies did you install for this project?'
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Please list the steps to use this'
+    }
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, function(err){
+        err ? console.log(err): console.log('README.md successfully created')
+    });
+}
+
+// TODO: Create a function to initialize app
+function init() {
+    inquirer.prompt(questions)
+    .then(function(data) {
+        writeToFile('README.md', generateMarkdown(data));
+        console.log(data);
+    });
+}
+
+// Function call to initialize app
+init();
